b95bad2ad0ad22b0953015879e7a89a07b16804b
vreznv <vrezn@me.com>
2023-06-05 23:48:36 +0300

    The 5th step

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Maintainability](https://api.codeclimate.com/v1/badges/95190cc75a005c15f0c3/maintainability)](https://codeclimate.com/github/vreznv/frontend-project-44/maintainability)
 [![Actions Status](https://github.com/vreznv/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/vreznv/frontend-project-44/actions)
+Check out a demo of the brain-even game https://asciinema.org/a/NDGo98FtSL8B8xKWXH5SWg4kA.
---
+++ bin/brain-even.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import greetUser from '../src/cli.js';
+
+const game = () => {
+  const name = greetUser();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.floor(Math.random() * 100);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer: ');
+    const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};
+
+game();
